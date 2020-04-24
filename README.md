11c97d56710ce8e717ed77ff9646705f23f28689
Frank Gerold <FrankETSGerold@gmail.com>
2020-04-24 14:37:05 -0400

    Initializing repo for node tutorial code along. Dependencies managed by yarn 2.0. Includes Node gitignore template from github.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Note App
+
+Making a CLI note-taking app exclisively within Node.js.
+
+Code-along with @andrewjmead's complete Node Udemy course
---
+++ app.js
@@ -0,0 +1,71 @@
+// const fs = require('fs')
+// fs.writeFileSync('notes.txt', '420 blaze it!')
+
+const notes = require('./notes.js');
+const validator = require('validator')
+// import validator from 'validator';
+const chalk = require('chalk')
+const yargs = require('yargs')
+
+// const command = process.argv[2]
+
+// console.log(notes());
+// console.log(validator.isEmail('frank@mail.com'));
+// console.log(validator.isEmail('frankmail.com'));
+// console.log(validator.isURL('frankmail.com'));
+
+// customize yargs erison
+yargs.version('1.1.0')
+
+// add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note!',
+  handler: (argv) => console.log('Adding note!\nTitle:', argv.title, "\nBody: ", argv.body),
+  builder: {
+    title: {
+      describe: 'Note Title',
+      demandOption: true,
+      type: 'string',
+      alias: 't'
+    },
+    body: {
+      describe: 'Body of Note',
+      type: 'string',
+      demandOption: true,
+      alias: 'b'
+    }
+  }
+})
+
+// Remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a notwe!',
+  handler: () => console.log('removing that note')
+})
+
+// list command
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: () => console.log('List of notes:')
+})
+
+// read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: () => console.log('reading note now:')
+})
+
+console.log(chalk.red.bgBlue.bold('Cool Dude Alert'));
+console.log('Process arguments:', process.argv);
+console.log('Yargs arguments:', yargs.argv);
+
+// if (command === 'add') {
+//   console.log('Adding note!');
+// }
+// else if (command === 'remove') {
+//   console.log('Removing note!');
+// }
